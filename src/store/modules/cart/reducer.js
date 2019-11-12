e6759e83fd070227cb785d6db160b0ce9fb23ae5
Daniel Garcia <daniel.of@gmail.com>
2019-11-12 10:25:06 -0300

    Using Immer and controling Procuts Amount in cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,8 +1,32 @@
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
     case 'ADD_TO_CART':
-      return [...state, { ...action.product, amount: 1 }];
+      /** o produce recebe o estado atual e o draft é o rascunho
+       * do estado que pode ser modificado */
+      return produce(state, draft => {
+        // verificação da existencia do produto no carrinho
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.product, amount: 1 });
+        }
+      });
     default:
       return state;
   }
 }
+
+/**
+ * SEM UTILIZAR O IMMER
+ * */
+// export default function cart(state = [], action) {
+//   switch (action.type) {
+//     case 'ADD_TO_CART':
+//       return [...state, { ...action.product, amount: 1 }];
+//     default:
+//       return state;
+//   }
+// }
