90c6abd6c5fbf4551b6b4d4c47f2856ad73870f8
Daniel Garcia <daniel.of@gmail.com>
2019-11-18 14:27:42 -0300

    Stock verification

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -11,8 +11,20 @@ function* addToCart({ id }) {
     state.cart.find(p => p.id === id)
   );
 
+  /** VERIFICAÇÕES DE ESTOQUE */
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
   if (productExists) {
-    const amount = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
     // yield funciona de forma similar ao await
