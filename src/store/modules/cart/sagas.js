fad9129c3211e4b3e1a1952ae7c73e86e96d9c91
Daniel Garcia <daniel.of@gmail.com>
2019-11-18 14:38:27 -0300

    using React-Toastify

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,6 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
 import api from '../../../services/api';
 import { formatPrice } from '../../../util/format';
 import { addToCartSuccess, updateAmount } from './action';
@@ -20,7 +22,7 @@ function* addToCart({ id }) {
   const amount = currentAmount + 1;
 
   if (amount > stockAmount) {
-    console.tron.warn('ERRO');
+    toast.error('Quantidade solicitada fora de estoque!');
     return;
   }
 
